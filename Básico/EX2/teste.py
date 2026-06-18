4bef6e5139090acbc73ff7f53d6d0a08578e28e1
Thiago Saraiva <thiagosaraiva.dev.br@gmail.com>
2026-06-17 23:48:50 -0300

    Primeira pergunta com Python

---
+++ Básico/EX2/teste.py
@@ -0,0 +1,11 @@
+nome = input("Qual seu nome?")
+print("Faala," + nome + "! Você está bem?")
+
+
+
+beleza = input("Beleza?")
+print(beleza)
+if beleza == "Sim" or beleza == "sim" or beleza == "SiM" or beleza == "SIM"or beleza == "sIM"  or beleza == "SIm" or beleza == "siM" or beleza == " Sim":
+    print("Que bom que você está bem!")
+else :
+    print("Espero que você melhore logo!")
